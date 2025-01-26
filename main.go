e58d494c822ba6ed46219ded97dbe55ad7b3515b
Adam Fitzgerald <adam@networknoob.net>
2025-01-26 23:24:51 +0000

    change default to http port 80

--- main.go
+++ main.go
@@ -188,7 +188,7 @@ func main() {
 
 	// Serve the page
 	http.HandleFunc("/", pageHandler)
-	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Starting server on :80")
+	log.Fatal(http.ListenAndServe(":80", nil))
 
 }
